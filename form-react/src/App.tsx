696e44194c0852914f1cb66311f116d875312487
Alexander <renyreny125tt@gmail.com>
2022-08-10 20:13:54 +0300

    feat: start project react form validation

---
+++ form-react/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './App.module.scss';
+import { Form } from './components/form/Form';
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ form-react/src/components/form/Form.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './Form.module.scss';
+
+export function Form() {
+  return (
+    <div className={styles.Form}>
+    </div>
+  );
+}
